259a5aba23f1c20a65d5cc6b078707dad79c8645
Zooknee <zunnbugg@gmail.com>
2025-09-28 15:59:37 -0700

    Added calculator, user input, and condition scripts

---
+++ day3_variables.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+fav_number = int(input("What is your favorite number? "))
+
+double_number = fav_number * 2
+
+print(f"Hello {name}, you are {age} years old.")
+print(f"Your favorite number doubled is {double_number}.")
---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input ("How old are you? ")
+language = input("Favorite program language? ")
+print("Hello", name + "!", "You are", age, "years old", "and you love", language + ".")
