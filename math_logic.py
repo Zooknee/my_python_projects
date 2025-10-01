196ead5aabd5e6b8cb0f67fa26eec257f9823107
Zooknee <zunnbugg@gmail.com>
2025-10-01 00:07:10 -0700

    Day 5: math and logic script

--- math_logic.py
+++ math_logic.py
@@ -3,6 +3,7 @@ num2 = int(input("Enter second number: "))
 
 print("Sum =", num1 + num2)
 print("Difference =", num1 - num2)
+print("Product =", num1 * num2)
 
 if num1 > num2:
     print("The bigger number is", num1)
