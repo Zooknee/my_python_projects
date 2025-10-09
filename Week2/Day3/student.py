642b93c7fdf032a50be22142934323532a2b386c
zooknee <zunnbugg@gmail.com>
2025-10-08 22:49:34 -0700

    Day 3 Dictionaries practice.

---
+++ Week2/Day3/student.py
@@ -0,0 +1,7 @@
+student = {"name": "Isaiah", "age": 25, "course": "Backend Development"}
+student["age"] = 26
+student["grade"] = "A"
+print(student.get("city", "Not specified"))
+print(student)
+print(student.keys())
+print(student.values())
