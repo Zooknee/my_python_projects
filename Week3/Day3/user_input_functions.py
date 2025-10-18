4719d226f172c1d6167c7e638fc32e72245b7c96
zooknee <zunnbugg@gmail.com>
2025-10-17 17:21:16 -0700

    Add Day3 functions with user inputs

---
+++ Week3/Day3/user_input_functions.py
@@ -0,0 +1,16 @@
+def greet_user():
+    name = input('Enter your name: ')
+    print(f"Hello, {name}! Welcome to Python")
+
+greet_user()
+
+def personalized_greeting():
+    name = input('What is your name? ')
+    mood = input('How are you feeling today? ')
+    print(f"Hi {name}! It's great that you're feeling {mood}.")
+
+personalized_greeting()
+
+def fav_color():
+    color = input('What is your favorite color?: ')
+    print(f'Your favorite color is {color}.')
